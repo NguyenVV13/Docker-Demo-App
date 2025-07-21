59d946a6fc1534b9debaa2d6aec006e401c57523
Vi Nguyen <nguyenvv13@gmail.com>
2025-07-21 19:24:21 -0400

    Modified everything to work in its base form

---
+++ README.md
@@ -0,0 +1,4 @@
+A simple webapp that takes user input in text fields and updates the website with it, following a Docker development tutorial by TechWorld with Nana on YouTube.
+https://www.youtube.com/watch?v=3c-iBn73dDE
+Uses HTML, CSS, and JavaScript for the frontend
+Uses Node.js with Express module and MongoDB for the backend, and is packaged together into an application with Docker.
--- server.js
+++ server.js
@@ -2,21 +2,21 @@
  * Node.js file for the app backend
  */
 
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
-app.get('/profile-picture', function (req, res) {
+app.get('/profile-picture', function(req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/cafe.jpg"));
   res.writeHead(200, {'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-app.listen(3000, function () {
+app.listen(3000, function() {
   console.log("app listening on port 3000!");
 });
