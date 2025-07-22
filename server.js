ec25846ed059fac67e15c744bc118e43ce28d4f1
Vi Nguyen <nguyenvv13@gmail.com>
2025-07-22 02:14:31 -0400

    Fix code formatting, currently a filenotfound bug

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const app = express();
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
-  });
+});
 
 app.get('/profile-picture', function(req, res) {
-  let img = fs.readFileSync(path.join(__dirname, "images/cafe.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
-  res.end(img, 'binary');
+    let img = fs.readFileSync(path.join(__dirname, "images/cafe.jpg"));
+    res.writeHead(200, {'Content-Type': 'image/jpg' });
+    res.end(img, 'binary');
 });
 
 app.listen(3000, function() {
-  console.log("app listening on port 3000!");
+    console.log("app listening on port 3000!");
 });
